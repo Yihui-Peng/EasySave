dfd427267d2fb14c75f15a61e83ad1226b48184d
H.Chen <s3990788@vuw.leidenuniv.nl>
2024-10-20 00:48:45 +0200

    new css file and login page

--- app.py
+++ app.py
@@ -4,23 +4,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', active_page='home')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 @app.route('/newSpending')
 def newSpending():
-    return render_template('newSpending.html')
+    return render_template('newSpending.html', active_page='newSpending')
 
 @app.route('/savingGoal')
 def savingGoal():
-    return render_template('savingGoal.html')
+    return render_template('savingGoal.html', active_page='savingGoal')
 
 @app.route('/setting')
 def setting():
-    return render_template('settings.html')
+    return render_template('settings.html', active_page='setting')
 
 @app.route('/userProfile')
 def userProfile():
-    return render_template('userProfile.html')
+    return render_template('userProfile.html', active_page='userProfile')
 
 @app.errorhandler(404)
 def page_not_found(e):
